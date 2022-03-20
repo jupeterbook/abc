e0f5f4fa229b390ccff0f0b06866fb3070e14b63
jupeterbook <1301480179@qq.com>
2022-03-20 20:58:45 +0800

    test

---
+++ app/src/main/java/com/example/theactivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.theactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("onCreate");
+    }
+    @Override
+    protected void onStart() {
+    super.onStart();
+    System.out.println("onStart");
+}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("onStop");
+    }
+
+    @Override
+    protected void onRestart() {        //重启
+        super.onRestart();
+        System.out.println("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {        //结束
+        super.onDestroy();
+        System.out.println("onDestroy");
+    }
+}
